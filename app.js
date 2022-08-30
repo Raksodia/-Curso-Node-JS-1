6231b13000dba765dac5294e7590e6754ad2af0c
Williamns Robles <robles.williamns@gmail.com>
2022-08-29 21:38:11 -0500

    desestructuracion
    desestructuracion

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+//require
+/*
 const saludos = require("./saludos.js");
 console.log(saludos.saludar("Bienvenido"));
 console.log(saludos.saludarHolaMundo());
+*/
+
+//desestructuracion
+const { saludar , saludarHolaMundo } = require("./saludos.js");
+console.log(saludar("Bienvenido"));
+console.log(saludarHolaMundo());
