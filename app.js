f4439354afab30a1b36d90c4fc7e67aab5d49006
Williamns Robles <robles.williamns@gmail.com>
2022-08-29 22:07:53 -0500

    Modulo Built In - Console
    Modulo Built In - Console

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ console.log(saludos.saludarHolaMundo());
 const { saludar , saludarHolaMundo } = require("./saludos.js");
 console.log(saludar("Bienvenido"));
 console.log(saludarHolaMundo());
+
+//Modulo Built In - Console
+console.warn("Warn Ocurrio un Error");
+console.error("Error Ocurrio un Error");
+console.error(new Error("Ocurrio un Error"));
